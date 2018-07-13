67627a7d15def3c86dda38b8bba2a35dafa72a67
start0821 <Ktaesu@gmail.net>
2018-07-13 14:02:43 +0900

    binomial 추가

--- week2.py
+++ week2.py
@@ -1,16 +1,16 @@
-# Normal Distribution, Beta Distribution
+# Normal Distribution, Beta Distribution, Binomial Distribution
 import math
 import numpy as np
 
 
 class NormalDistribution:
     def __init__(self,average=0,variance=1):
-        self.average = average
+        self.mean = average
         self.variance = variance
         self.distribution = np.vectorize(self.distribution)
 
     def distribution(self,x=0):
-        return (1./(self.variance*math.sqrt(math.pi*2.)))*math.exp(-(x-self.average)**2/(2.*self.variance**2))
+        return (1./(self.variance*math.sqrt(math.pi*2.)))*math.exp(-(x-self.mean)**2/(2.*self.variance**2))
 
 def N(x,average=0,variance=1):
     return (1./(variance*math.sqrt(math.pi*2.)))*math.exp(-(x-average)**2/(2.*variance**2))
@@ -23,9 +23,11 @@ class BetaDistribution:
         self.beta = beta
         self.B = math.gamma(self.alpha)*math.gamma(self.beta)/math.gamma(self.beta+self.alpha)
         self.distribution = np.vectorize(self.distribution)
+        self.mean = self.alpha/(self.alpha+self.beta)
+        self.variance = self.alpha*self.beta/((self.alpha+self.beta)**2*(self.alpha+self.beta+1))
 
     def distribution(self,x):
-        return (x**self.alpha*(1-x)**self.beta)/self.B
+        return (x**(self.alpha-1)*(1-x)**(self.beta-1))/self.B
 
 def Beta(x,alpha,beta):
     B = math.gamma(alpha)*math.gamma(beta)/math.gamma(beta+alpha)
@@ -33,12 +35,26 @@ def Beta(x,alpha,beta):
 Beta = np.vectorize(Beta)
 
 
+class BinomialDistribution:
+    def __init__(self,n,p):
+        self.p = p
+        self.n = n
+        self.distribution = np.vectorize(self.distribution)
+        self.mean = self.n*self.p
+        self.variance = self.n*self.p*(1-self.p)
+
+    def distribution(self,x):
+        return math.factorial(self.n)/(math.factorial(x)*math.factorial(self.n-x)) * (self.p**x) * ((1-self.p)**(self.n-x))
+
+def Bi(x,alpha,beta):
+    return math.factorial(self.n)/(math.factorial(x)*math.factorial(self.n-x)) * (self.p**x) * ((1-self.p)**(self.n-x))
+Bi = np.vectorize(Beta)
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    Dis = BetaDistribution(.5,.5)
-    X = np.array(list(range(0,1000)))/1000
+    Dis = BinomialDistribution(100,.2)
+    X = np.array(list(range(1,100)))
     Y = Dis.distribution(X)
-    plt.plot(X,Y)
+    plt.plot(X,Y,'o')
     plt.show()
-    print(Dis.distribution(.2))
